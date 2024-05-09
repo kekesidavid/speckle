78b3c11702efd8a9827c3296a0c35144a5583b5e
unknown <david.kekesi@gmail.com>
2024-05-09 10:18:23 +0200

    fix: returning pointer to local variable

--- AddOns/Speckle/Sources/AddOn/ArchiCadApiException.h
+++ AddOns/Speckle/Sources/AddOn/ArchiCadApiException.h
@@ -1,23 +1,22 @@
-#include <iostream>
 #include <exception>
 #include <string>
 
-class ArchiCadApiException : public std::exception 
+class ArchiCadApiException : public std::exception
 {
 private:
     int errorCode;
+    std::string message;
 
 public:
-    ArchiCadApiException(int code) : errorCode(code) {}
+    ArchiCadApiException(int code) : errorCode(code), message("ArchiCad API Exception: Error code " + std::to_string(errorCode)) {}
 
-    virtual const char* what() const noexcept override 
-	{
-        std::string message = "ArchiCad API Exception: Error code " + std::to_string(errorCode);
+    virtual const char* what() const noexcept override
+    {
         return message.c_str();
     }
 
-    int getErrorCode() const noexcept 
-	{
+    int getErrorCode() const noexcept
+    {
         return errorCode;
     }
 };
